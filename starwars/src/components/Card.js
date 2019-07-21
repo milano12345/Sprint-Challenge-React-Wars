e11424e6a7efc323634390c120be6cbc49f91d1f
milano12345 <dmilano857@gmail.com>
2019-07-21 11:00:33 -0600

    updated readme, updated card/card data. Rendered items to screen

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Card = (props) => {
     return <div>
-        <div>{props.data.name}</div>
-            <div> </div>
+            <div>Name: {props.data.name}</div>
+            <div>Height: {props.data.height}</div>
+            <div>Gender: {props.data.gender} </div>
+            <div>Hair Color: {props.data.hair_color} </div>
+            <div>Eye Color: {props.data.eye_color}</div>
             </div>
 
 }
--- starwars/src/components/CardData.js
+++ starwars/src/components/CardData.js
@@ -2,16 +2,16 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Card from './Card';
 
-function CardData() {
+const CardData = () => {
     const [starData, setStarData] = useState([]);
     console.log(starData)
     useEffect(() => {
     axios
         .get('https://swapi.co/api/people/')
-        .then(res => console.log(res))
+        .then(res => setStarData(res.data.results))
         .catch(err => console.log(err))
     },[])
-    return <Card data={starData}/>
+    return starData.map(char => <Card data={char}/>)
 }
 
 export default CardData;
